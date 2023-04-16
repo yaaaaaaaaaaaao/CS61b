034977dd6dbaf59843265bc58911ab57fc974392
Yao Wang <yaow17c@gmail.com>
2023-04-15 22:43:45 -0700

    Lab2 completed

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -10,7 +10,7 @@ public class IntListExercises {
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
-        while (head.rest != null) {
+        while (head != null) {
             head.first += c;
             head = head.rest;
         }
@@ -33,7 +33,9 @@ public class IntListExercises {
         }
     }
 
-    /** Returns the max value in the IntList starting at L. */
+    /**
+     * Returns the max value in the IntList starting at L.
+     */
     public static int max(IntList L) {
         int max = L.first;
         IntList p = L.rest;
@@ -46,12 +48,13 @@ public class IntListExercises {
         return max;
     }
 
-    /** Returns true if the last digit of x is equal to
-     *  the first digit of x.
+    /**
+     * Returns true if the last digit of x is equal to
+     * the first digit of x.
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
-        while (x > 10) {
+        while (x >= 10) {
             x = x / 10;
         }
         int firstDigit = x % 10;
@@ -65,18 +68,29 @@ public class IntListExercises {
      * @param lst IntList from Lecture
      * @return True if there was an update to the list
      */
+
     public static boolean squarePrimes(IntList lst) {
         // Base Case: we have reached the end of the list
-        if (lst == null) {
-            return false;
+//        boolean currElemIsPrime = false;
+//        if (lst == null) {
+//            return currElemIsPrime;
+//        }
+//
+//        if (Primes.isPrime(lst.first)) {
+//            lst.first *= lst.first;
+//            currElemIsPrime = true;
+//        }
+//        squarePrimes(lst.rest);
+//        return currElemIsPrime;
+        boolean currElemIsPrime = false;
+        IntList L = lst;
+        while (L != null) {
+            if (Primes.isPrime(L.first)) {
+                L.first *= L.first;
+                currElemIsPrime = true;
+            }
+            L = L.rest;
         }
-
-        boolean currElemIsPrime = Primes.isPrime(lst.first);
-
-        if (currElemIsPrime) {
-            lst.first *= lst.first;
-        }
-
-        return currElemIsPrime || squarePrimes(lst.rest);
+        return currElemIsPrime;
     }
 }
